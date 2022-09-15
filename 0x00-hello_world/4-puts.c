cda0f8a9ae698fcd2b54f9fddc7ad556e15a2336
gavintobin <gavintobin714@gmail.com>
2022-09-15 09:30:09 -0700

    fixed bettys issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
  #include <stdio.h>
 
 /**
- *  * main - entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
+  * main - entry point
+  *
+  * Return: Always 0 (success)
+  **/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+		puts(/*"Programming is like building a multilingual puzzle*/);
 			return (0);
 }
