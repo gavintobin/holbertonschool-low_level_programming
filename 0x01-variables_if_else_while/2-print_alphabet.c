cb579408b4ca962dbaf3a06a4b6aa9b0914c7b2b
gavintobin <gavintobin714@gmail.com>
2022-09-16 12:52:39 -0700

    try1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *  Return: Always (0) sucess
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar (ch);
+	}
+	return (0);
+}
