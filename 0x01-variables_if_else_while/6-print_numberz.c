d7eca373d6d5df812e06a800d7a68e032a727c3d
gavintobin <gavintobin714@gmail.com>
2022-09-17 14:36:11 -0700

    same issue

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	char num;
+	char letter;
 
-	for (num = 0 ; num <= 9 ; num++)
-		putchar (num);
+	for (letter = 0 ; letter <= 9 ; letter++)
+		putchar (letter);
 	{
 		putchar ('\n');
 	}
