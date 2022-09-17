223a72de15de3919e0c2aa9163f390bf98f7781c
gavintobin <gavintobin714@gmail.com>
2022-09-17 14:24:35 -0700

    try1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base ten
+ * Return: Always (0) sucess
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 0 ; ch <= 9 ; ch++)
+		putchar (ch);
+	{
+		putchar ('\n');
+	}
+	return (0);
+}
