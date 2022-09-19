2a5e6fedf4efc8c616926210207f52a257902103
gavintobin <gavintobin714@gmail.com>
2022-09-19 07:15:40 -0700

    try1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program will print putchar followed by new line
+ *  Return: Always (0) sucess
+ */
+int main(void)
+{
+	char z[100] = "_putchar";
+
+	printf("%s\n", z);
+	return (0);
+}
+
