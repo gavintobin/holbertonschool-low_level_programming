8afc14e150ab27c805a7162d59bfb3a130831fdf
gavintobin <gavintobin714@gmail.com>
2022-09-19 10:49:23 -0700

    try 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  print_alphabet_x10 - prints ten times alphabet
+ *  Return:Always (0) sucess
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	char d;
+
+	for (c = 0 ; c <= 10 ; c++)
+	{
+		for (d = 'a' ; d <= 'z' ; d++)
+		{
+			_putchar(d);
+		}
+		_putchar('\n');
+	}
+}
