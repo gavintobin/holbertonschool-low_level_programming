936af2eb43749fb0db821fd6519533c8007e31c3
gavintobin <gavintobin714@gmail.com>
2022-09-22 13:59:11 -0700

    pleased betty

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - will print a square in term
+ * @size: size of square
+ * Return: Always (0)
+ */
+void print_square(int size)
+{
+	int i;
+	int n;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+	for (i = 0 ; i < size ; i++)
+	{
+		for (n = 0 ; n < size ; n++)
+		{
+			_putchar(35);
+			_putchar(10);
+		}
+	}
+	}
+}
+
