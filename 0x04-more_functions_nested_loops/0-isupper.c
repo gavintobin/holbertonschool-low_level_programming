9289671458e4247fae352bec66815c534593fef1
gavintobin <gavintobin714@gmail.com>
2022-09-22 07:40:39 -0700

    try1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ *@c: int for number
+ * Return: (1) uppercase  (0) else
+ */
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
