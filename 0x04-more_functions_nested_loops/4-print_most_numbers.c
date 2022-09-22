108d1326583300f71243b6f42f0bcf15dabb1daf
gavintobin <gavintobin714@gmail.com>
2022-09-22 10:50:22 -0700

    pleased betty

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all numbers asked except some
+ *
+ * Return: Always (0)
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef _MAIN_H_
 #define  _MAIN_H_
 #include <unistd.h>
-int main(void);
 
 
 /**
