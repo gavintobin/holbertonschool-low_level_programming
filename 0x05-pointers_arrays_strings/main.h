f558944d2ceb3bb32d101791517a808d1c068e6c
gavintobin <gavintobin714@gmail.com>
2022-09-23 08:19:19 -0700

    fixed dir name

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+/**
+ * protptype file for last project
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#endif
+
