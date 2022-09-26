4c5cf472c8f63584d4f92a62a895ba0710e216ae
gavintobin <gavintobin714@gmail.com>
2022-09-26 07:56:57 -0700

    try1

---
+++ 0x05-pointers_arrays_strings/print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse 
+ * @s: i guess sttring
+ * Return: Always (0)
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	
+	for (i = 0 ; s[i] !=
+	{
+		if (str[i] ==
