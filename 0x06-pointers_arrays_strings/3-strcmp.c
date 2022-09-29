7880e3cb233d4bfc5c6a3479d3b9220ffb1c2d98
gavintobin <gavintobin714@gmail.com>
2022-09-29 11:35:30 -0700

    try1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int len;
 
-	for( len = 0 ; s1[len] && s2[len] ;)
+	for (len = 0 ; s1[len] && s2[len] ;)
 	{
 		if (s1[len] == s2[len])
 			len++;
 		else
 			break;
 	}
-	return(s1[len] - s2[len]);
+	return (s1[len] - s2[len]);
 }
