0f9dafd371411b311f1584a186f222a0b17a38aa
gavintobin <gavintobin714@gmail.com>
2022-10-10 08:12:56 -0700

    fixed code

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguements
+ * @argv: arg vec
+ * @argc: arg count
+ * Return:(0)
+ */
+
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int n;
+
+	for (n = 0 ; n < argc ; n++)
+	{
+		printf("%d\n ", n - 1);
+	}
+	return (0);
+}
