c5384773701f1bf690b8e09654a42791dcbada81
gavintobin <gavintobin714@gmail.com>
2022-10-25 14:56:53 -0700

    first

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,8 +17,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		return;
 	for (i = 0 ; i < n ; i++)
 	{
 		str = va_arg(args, char *);
