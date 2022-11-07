77e39da26168aeee061485265e399d0908266537
gavintobin <gavintobin714@gmail.com>
2022-11-07 09:13:55 -0800

    try1

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+ * get_nodeint - returns nth node
+ * @head: pointer
+ * @index: beginning of node
+ * Return: null
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp = head;
+	unsigned int i = 0;
+
+	while (temp)
+	{
+		if ( i == index)
+			return (temp);
+		temp = temp->next;
+		i++;
+	}
+	return (NULL);
+}
