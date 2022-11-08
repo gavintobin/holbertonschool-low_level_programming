d8ded912a28530c69dbe82cbeaa9d83eb765d178
gavintobin <gavintobin714@gmail.com>
2022-11-07 21:26:47 -0800

    try1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <string.h>
-#include "_pow.c"
+
 /**
  * binary_to_uint - converts binary to un int
  * @b: pointer to binary string
@@ -9,18 +9,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
-	unsigned int dec = 0;
+	unsigned int num;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	while (b[i])
+
+	for(num = 0 ; *b ; b++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*b == '1')
+			num = (num << 1) | 1;
+		else if (*b == '0')
+			num = num << 1;
+		else
 			return (0);
-		if (b[i] == '1')
-			dec += mypow(2, strlen(b) - i - 1);
-		i++;
 	}
-	return (dec);
+	return (num);
 }
