e78a4e1ec706e5126e9640cbcc8a3ef8359fd559
gavintobin <gavintobin714@gmail.com>
2022-11-08 19:40:14 -0800

    try1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file
+ * @text_content: text
+ * Return: stuff
+ */
+
+int create_file(const char *filename, char *text_content);
+{
+	int fd;
+
+	fd = open(filename, O_CREAT |O_WRONLY | O_TRUNG, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		if (write(fd, text_content, strlen(text_content)) == -1)
+			return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
