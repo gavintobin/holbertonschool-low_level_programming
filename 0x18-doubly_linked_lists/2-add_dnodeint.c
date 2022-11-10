7d2038d1b0555d249cad662e500cb2db08d98737
gavintobin <gavintobin714@gmail.com>
2022-11-10 07:39:48 -0800

    try1

---
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds nn at beginning
+ * @head: pointer to pointer
+ * @n: int
+ * Return: nn
+ */
+
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_s *nn = malloc(sizeof(nn));
+	dlistint_s *tail;
+
+	nn->n = n;
+
+	if (*head == NULL)
+	{
+		(*head) = tail = nn;
+		(*head)->prev = NULL;
+		tail->next = NULL;
+	}
+	else
+	{
+		(*head)->prev = nn;
+		nn->next = (*head);
+		nn->prev = NULL;
+		(*head) = nn;
+	}
+	return (nn);
+}
+
