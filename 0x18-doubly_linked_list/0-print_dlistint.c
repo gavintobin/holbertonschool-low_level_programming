ad9acccdaf7aaec99d3c02fabf03e48eb16f42a4
gavintobin <gavintobin714@gmail.com>
2022-11-10 07:14:05 -0800

    try1

---
+++ 0x18-doubly_linked_list/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints lements of list
+ * @h: pointer
+ * Return: num of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int e = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", n);
+		h = h->next;
+		e++;
+	}
+	return (e);
+}
