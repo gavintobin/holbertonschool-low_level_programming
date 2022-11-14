b3064067965e21c63869fda947684257414b86c4
gavintobin <gavintobin714@gmail.com>
2022-11-14 09:51:15 -0800

    try1

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: table to delete
+ * Return: none
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr;
+
+	for (i = 0 ; i < ht->size ; i++)
+	{
+		if (ht->array[i])
+		{
+			while (ht->array[i])
+			{
+				curr = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = curr;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
