8968043f614d4e18aed0d95e8f0bdf601b78fa3e
gavintobin <gavintobin714@gmail.com>
2022-11-14 09:33:46 -0800

    try1

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives index of a key
+ * @key: pointer to key
+ * @size: size of key
+ * Return: index key is stored in
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	return (hash_djb2(key) % size);
+}
